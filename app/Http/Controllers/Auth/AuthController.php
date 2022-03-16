39d41aaa85648e5cd70af943faebe6f9b0343e47
Andrés Camilo Yáñez Escobar <andrescamiloye@ufps.edu.co>
2022-03-16 11:03:58 -0500

    Login and logout completed.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -34,11 +34,46 @@ class AuthController extends Controller
 
         $user = User::Where('email', $credentials['email'])->first();
 
-        if (!Auth::attempt($credentials)) {
+        if (!empty($user) && !Hash::check($credentials['password'], $user->password)) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
-        // if (!empty($user) && !Hash::check($credentials['password'], $user->password)) {
-        //     return response()->json(['message' => 'Unauthorized'], 401);
-        // }
+
+        $apiToken = Str::random(64);
+
+        $user->api_token = $apiToken;
+        $user->save();
+
+        return response()->json(['message' => 'Success!', 'user' => [
+            'name' => $user->name,
+            'email' => $user->email,
+        ], 'token' => base64_encode($apiToken)], 401);
+    }
+
+
+    /**
+     * Get the Auth User information.
+     * 
+     * @return Response
+     */
+    public function profile()
+    {
+        return response()->json(['data' => Auth::user()]);
+    }
+
+    /**
+     * Loggout the Auth User.
+     * 
+     * @return Response
+     */
+    public function logout(Request $request)
+    {
+        $token = base64_decode($request->header('Authorization'));
+
+        $user = User::where('api_token', $token)->first();
+
+        $user->api_token = null;
+        $user->save();
+
+        return response()->json(['message' => 'Success! Good Bye.']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,85 +20,92 @@ $router->get('/', function () use ($router) {
 
 $router->group(['namespace' => 'Auth'], function () use ($router) {
     $router->post('login', ['as' => 'auth.login', 'uses' => 'AuthController@login']);
-});
-
-$router->group(['prefix' => 'configuration'], function () use ($router) {
-    $router->group(['prefix' => 'localization', 'namespace' => 'Localization'], function () use ($router) {
-        $router->group(['prefix' => 'countries'], function () use ($router) {
-            $router->get('/', ['as' => 'countries.index', 'uses' => 'CountryController@index']);
-            $router->post('/', ['as' => 'countries.store', 'uses' => 'CountryController@store']);
-            $router->group(['prefix' => '/{id}'], function () use ($router) {
-                $router->get('/', ['as' => 'countries.show', 'uses' => 'CountryController@show']);
-                $router->put('/', ['as' => 'countries.update', 'uses' => 'CountryController@update']);
-                $router->delete('/', ['as' => 'countries.delete', 'uses' => 'CountryController@destroy']);
-            });
-        });
-        $router->group(['prefix' => 'states'], function () use ($router) {
-            $router->get('/', ['as' => 'states.index', 'uses' => 'StateController@index']);
-            $router->post('/', ['as' => 'states.store', 'uses' => 'StateController@store']);
-            $router->group(['prefix' => '/{id}'], function () use ($router) {
-                $router->get('/', ['as' => 'states.show', 'uses' => 'StateController@show']);
-                $router->put('/', ['as' => 'states.update', 'uses' => 'StateController@update']);
-                $router->delete('/', ['as' => 'states.delete', 'uses' => 'StateController@destroy']);
-            });
-        });
-        $router->group(['prefix' => 'cities'], function () use ($router) {
-            $router->get('/', ['as' => 'cities.index', 'uses' => 'CityController@index']);
-            $router->post('/', ['as' => 'cities.store', 'uses' => 'CityController@store']);
-            $router->group(['prefix' => '/{id}'], function () use ($router) {
-                $router->get('/', ['as' => 'cities.show', 'uses' => 'CityController@show']);
-                $router->put('/', ['as' => 'cities.update', 'uses' => 'CityController@update']);
-                $router->delete('/', ['as' => 'cities.delete', 'uses' => 'CityController@destroy']);
-            });
-        });
-    });
-    $router->group(['namespace' => 'Configuration'], function () use ($router) {
-        $router->group(['prefix' => 'room_types'], function () use ($router) {
-            $router->get('/', ['as' => 'room_types.index', 'uses' => 'RoomTypeController@index']);
-            $router->post('/', ['as' => 'room_types.store', 'uses' => 'RoomTypeController@store']);
-            $router->group(['prefix' => '/{id}'], function () use ($router) {
-                $router->get('/', ['as' => 'room_types.show', 'uses' => 'RoomTypeController@show']);
-                $router->put('/', ['as' => 'room_types.update', 'uses' => 'RoomTypeController@update']);
-                $router->delete('/', ['as' => 'room_types.delete', 'uses' => 'RoomTypeController@destroy']);
-            });
-        });
-        $router->group(['prefix' => 'accommodation_types'], function () use ($router) {
-            $router->get('/', ['as' => 'accommodation_types.index', 'uses' => 'AccommodationTypeController@index']);
-            $router->post('/', ['as' => 'accommodation_types.store', 'uses' => 'AccommodationTypeController@store']);
-            $router->group(['prefix' => '/{id}'], function () use ($router) {
-                $router->get('/', ['as' => 'accommodation_types.show', 'uses' => 'AccommodationTypeController@show']);
-                $router->put('/', ['as' => 'accommodation_types.update', 'uses' => 'AccommodationTypeController@update']);
-                $router->delete('/', ['as' => 'accommodation_types.delete', 'uses' => 'AccommodationTypeController@destroy']);
-            });
-        });
-        $router->group(['prefix' => 'assignment_room_types'], function () use ($router) {
-            $router->get('/', ['as' => 'assignment_room_types.index', 'uses' => 'AssignmentRoomTypeController@index']);
-            $router->post('/', ['as' => 'assignment_room_types.store', 'uses' => 'AssignmentRoomTypeController@store']);
-            $router->group(['prefix' => '/{id}'], function () use ($router) {
-                $router->get('/', ['as' => 'assignment_room_types.show', 'uses' => 'AssignmentRoomTypeController@show']);
-                $router->put('/', ['as' => 'assignment_room_types.update', 'uses' => 'AssignmentRoomTypeController@update']);
-                $router->delete('/', ['as' => 'assignment_room_types.delete', 'uses' => 'AssignmentRoomTypeController@destroy']);
-            });
-        });
+    $router->group(['middleware' => 'auth', 'prefix' => 'auth'], function () use ($router) {
+        $router->get('profile', ['as' => 'auth.profile', 'uses' => 'AuthController@profile']);
+        $router->get('logout', ['as' => 'auth.logout', 'uses' => 'AuthController@logout']);
     });
 });
 
-$router->group(['prefix' => 'hotels'], function () use ($router) {
-    $router->get('/', ['as' => 'hotels.index', 'uses' => 'HotelController@index']);
-    $router->post('/', ['as' => 'hotels.store', 'uses' => 'HotelController@store']);
-    $router->group(['prefix' => '/{id}'], function () use ($router) {
-        $router->get('/', ['as' => 'hotels.show', 'uses' => 'HotelController@show']);
-        $router->put('/', ['as' => 'hotels.update', 'uses' => 'HotelController@update']);
-        $router->delete('/', ['as' => 'hotels.delete', 'uses' => 'HotelController@destroy']);
-    });
-});
 
-$router->group(['prefix' => 'rooms'], function () use ($router) {
-    $router->get('/', ['as' => 'rooms.index', 'uses' => 'RoomController@index']);
-    $router->post('/', ['as' => 'rooms.store', 'uses' => 'RoomController@store']);
-    $router->group(['prefix' => '/{id}'], function () use ($router) {
-        $router->get('/', ['as' => 'rooms.show', 'uses' => 'RoomController@show']);
-        $router->put('/', ['as' => 'rooms.update', 'uses' => 'RoomController@update']);
-        $router->delete('/', ['as' => 'rooms.delete', 'uses' => 'RoomController@destroy']);
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->group(['prefix' => 'configuration'], function () use ($router) {
+        $router->group(['prefix' => 'localization', 'namespace' => 'Localization'], function () use ($router) {
+            $router->group(['prefix' => 'countries'], function () use ($router) {
+                $router->get('/', ['as' => 'countries.index', 'uses' => 'CountryController@index']);
+                $router->post('/', ['as' => 'countries.store', 'uses' => 'CountryController@store']);
+                $router->group(['prefix' => '/{id}'], function () use ($router) {
+                    $router->get('/', ['as' => 'countries.show', 'uses' => 'CountryController@show']);
+                    $router->put('/', ['as' => 'countries.update', 'uses' => 'CountryController@update']);
+                    $router->delete('/', ['as' => 'countries.delete', 'uses' => 'CountryController@destroy']);
+                });
+            });
+            $router->group(['prefix' => 'states'], function () use ($router) {
+                $router->get('/', ['as' => 'states.index', 'uses' => 'StateController@index']);
+                $router->post('/', ['as' => 'states.store', 'uses' => 'StateController@store']);
+                $router->group(['prefix' => '/{id}'], function () use ($router) {
+                    $router->get('/', ['as' => 'states.show', 'uses' => 'StateController@show']);
+                    $router->put('/', ['as' => 'states.update', 'uses' => 'StateController@update']);
+                    $router->delete('/', ['as' => 'states.delete', 'uses' => 'StateController@destroy']);
+                });
+            });
+            $router->group(['prefix' => 'cities'], function () use ($router) {
+                $router->get('/', ['as' => 'cities.index', 'uses' => 'CityController@index']);
+                $router->post('/', ['as' => 'cities.store', 'uses' => 'CityController@store']);
+                $router->group(['prefix' => '/{id}'], function () use ($router) {
+                    $router->get('/', ['as' => 'cities.show', 'uses' => 'CityController@show']);
+                    $router->put('/', ['as' => 'cities.update', 'uses' => 'CityController@update']);
+                    $router->delete('/', ['as' => 'cities.delete', 'uses' => 'CityController@destroy']);
+                });
+            });
+        });
+        $router->group(['namespace' => 'Configuration'], function () use ($router) {
+            $router->group(['prefix' => 'room_types'], function () use ($router) {
+                $router->get('/', ['as' => 'room_types.index', 'uses' => 'RoomTypeController@index']);
+                $router->post('/', ['as' => 'room_types.store', 'uses' => 'RoomTypeController@store']);
+                $router->group(['prefix' => '/{id}'], function () use ($router) {
+                    $router->get('/', ['as' => 'room_types.show', 'uses' => 'RoomTypeController@show']);
+                    $router->put('/', ['as' => 'room_types.update', 'uses' => 'RoomTypeController@update']);
+                    $router->delete('/', ['as' => 'room_types.delete', 'uses' => 'RoomTypeController@destroy']);
+                });
+            });
+            $router->group(['prefix' => 'accommodation_types'], function () use ($router) {
+                $router->get('/', ['as' => 'accommodation_types.index', 'uses' => 'AccommodationTypeController@index']);
+                $router->post('/', ['as' => 'accommodation_types.store', 'uses' => 'AccommodationTypeController@store']);
+                $router->group(['prefix' => '/{id}'], function () use ($router) {
+                    $router->get('/', ['as' => 'accommodation_types.show', 'uses' => 'AccommodationTypeController@show']);
+                    $router->put('/', ['as' => 'accommodation_types.update', 'uses' => 'AccommodationTypeController@update']);
+                    $router->delete('/', ['as' => 'accommodation_types.delete', 'uses' => 'AccommodationTypeController@destroy']);
+                });
+            });
+            $router->group(['prefix' => 'assignment_room_types'], function () use ($router) {
+                $router->get('/', ['as' => 'assignment_room_types.index', 'uses' => 'AssignmentRoomTypeController@index']);
+                $router->post('/', ['as' => 'assignment_room_types.store', 'uses' => 'AssignmentRoomTypeController@store']);
+                $router->group(['prefix' => '/{id}'], function () use ($router) {
+                    $router->get('/', ['as' => 'assignment_room_types.show', 'uses' => 'AssignmentRoomTypeController@show']);
+                    $router->put('/', ['as' => 'assignment_room_types.update', 'uses' => 'AssignmentRoomTypeController@update']);
+                    $router->delete('/', ['as' => 'assignment_room_types.delete', 'uses' => 'AssignmentRoomTypeController@destroy']);
+                });
+            });
+        });
+    });
+
+    $router->group(['prefix' => 'hotels'], function () use ($router) {
+        $router->get('/', ['as' => 'hotels.index', 'uses' => 'HotelController@index']);
+        $router->post('/', ['as' => 'hotels.store', 'uses' => 'HotelController@store']);
+        $router->group(['prefix' => '/{id}'], function () use ($router) {
+            $router->get('/', ['as' => 'hotels.show', 'uses' => 'HotelController@show']);
+            $router->put('/', ['as' => 'hotels.update', 'uses' => 'HotelController@update']);
+            $router->delete('/', ['as' => 'hotels.delete', 'uses' => 'HotelController@destroy']);
+        });
+    });
+
+    $router->group(['prefix' => 'rooms'], function () use ($router) {
+        $router->get('/', ['as' => 'rooms.index', 'uses' => 'RoomController@index']);
+        $router->post('/', ['as' => 'rooms.store', 'uses' => 'RoomController@store']);
+        $router->group(['prefix' => '/{id}'], function () use ($router) {
+            $router->get('/', ['as' => 'rooms.show', 'uses' => 'RoomController@show']);
+            $router->put('/', ['as' => 'rooms.update', 'uses' => 'RoomController@update']);
+            $router->delete('/', ['as' => 'rooms.delete', 'uses' => 'RoomController@destroy']);
+        });
     });
 });
