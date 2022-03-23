d06d54eccb3f7e722544fe9e3774cb89282017c9
Andrés Camilo Yáñez Escobar <andrescamiloye@ufps.edu.co>
2022-03-23 10:02:13 -0500

    Fixing some bugs..

--- app/Http/Resources/Localization/Country/StatesByCountryResource.php
+++ app/Http/Resources/Localization/Country/StatesByCountryResource.php
@@ -16,6 +16,7 @@ class StatesByCountryResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'cities' => $this->cities()->count(),
